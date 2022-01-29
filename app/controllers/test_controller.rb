a05bc6300f4d8619712d94b106a9a85d55a04bb9
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-29 15:52:33 -0700

    Setup user authentication with Devise

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,6 @@
+# TODO: Delete me
+class TestController < ApplicationController
+  def index
+    render html: 'Hello'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'test#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+    end
 end
