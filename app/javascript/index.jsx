6384ade974126644654bdc07794377d157c34c9a
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-02-02 12:14:37 -0700

    Remove log

--- app/javascript/index.jsx
+++ app/javascript/index.jsx
@@ -4,7 +4,6 @@ import { render } from "react-dom";
 import App from "./components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello")
     render(
         <App />,
         document.getElementById("root")
