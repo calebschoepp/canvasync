aa20f4f47608c51d0146d6f99ee33cad042e0f09
Ryan Kortbeek <rakortbe@ualberta.ca>
2022-02-14 15:21:09 -0700

    Remove breaking import

--- app/javascript/index.jsx
+++ app/javascript/index.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import "./channels"
-
 
 document.addEventListener("DOMContentLoaded", () => {
     render(
