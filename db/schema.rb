b1f8c102e47812cd30875b0928b0de993aa6d27b
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-03-05 10:29:09 -0700

    Formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,91 +10,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_050306) do
-
-  create_table "diffs", force: :cascade do |t|
-    t.integer "layer_id", null: false
-    t.integer "seq"
-    t.json "data", default: "\"\\\"{}\\\"\"", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["layer_id"], name: "index_diffs_on_layer_id"
+ActiveRecord::Schema.define(version: 20_220_210_050_306) do
+  create_table 'diffs', force: :cascade do |t|
+    t.integer 'layer_id', null: false
+    t.integer 'seq'
+    t.json 'data', default: '""{}""', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['layer_id'], name: 'index_diffs_on_layer_id'
   end
 
-  create_table "layers", force: :cascade do |t|
-    t.integer "page_id", null: false
-    t.integer "writer_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["page_id"], name: "index_layers_on_page_id"
-    t.index ["writer_id"], name: "index_layers_on_writer_id"
+  create_table 'layers', force: :cascade do |t|
+    t.integer 'page_id', null: false
+    t.integer 'writer_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['page_id'], name: 'index_layers_on_page_id'
+    t.index ['writer_id'], name: 'index_layers_on_writer_id'
   end
 
-  create_table "notebooks", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'notebooks', force: :cascade do |t|
+    t.text 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.integer "number"
-    t.integer "notebook_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["notebook_id"], name: "index_pages_on_notebook_id"
-    t.index ["number", "notebook_id"], name: "index_pages_on_number_and_notebook_id", unique: true
+  create_table 'pages', force: :cascade do |t|
+    t.integer 'number'
+    t.integer 'notebook_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['notebook_id'], name: 'index_pages_on_notebook_id'
+    t.index %w[number notebook_id], name: 'index_pages_on_number_and_notebook_id', unique: true
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["name"], name: "index_roles_on_name"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.string 'resource_type'
+    t.integer 'resource_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
+    t.index ['name'], name: 'index_roles_on_name'
+    t.index %w[resource_type resource_id], name: 'index_roles_on_resource'
   end
 
-  create_table "user_notebooks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "notebook_id", null: false
-    t.boolean "is_owner"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["notebook_id"], name: "index_user_notebooks_on_notebook_id"
-    t.index ["user_id"], name: "index_user_notebooks_on_user_id"
+  create_table 'user_notebooks', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'notebook_id', null: false
+    t.boolean 'is_owner'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['notebook_id'], name: 'index_user_notebooks_on_notebook_id'
+    t.index ['user_id'], name: 'index_user_notebooks_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at", precision: 6
-    t.datetime "last_sign_in_at", precision: 6
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at', precision: 6
+    t.datetime 'remember_created_at', precision: 6
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at', precision: 6
+    t.datetime 'last_sign_in_at', precision: 6
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  create_table 'users_roles', id: false, force: :cascade do |t|
+    t.integer 'user_id'
+    t.integer 'role_id'
+    t.index ['role_id'], name: 'index_users_roles_on_role_id'
+    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
+    t.index ['user_id'], name: 'index_users_roles_on_user_id'
   end
 
-  add_foreign_key "diffs", "layers"
-  add_foreign_key "layers", "pages"
-  add_foreign_key "layers", "user_notebooks", column: "writer_id"
-  add_foreign_key "pages", "notebooks"
-  add_foreign_key "user_notebooks", "notebooks"
-  add_foreign_key "user_notebooks", "users"
+  add_foreign_key 'diffs', 'layers'
+  add_foreign_key 'layers', 'pages'
+  add_foreign_key 'layers', 'user_notebooks', column: 'writer_id'
+  add_foreign_key 'pages', 'notebooks'
+  add_foreign_key 'user_notebooks', 'notebooks'
+  add_foreign_key 'user_notebooks', 'users'
 end
