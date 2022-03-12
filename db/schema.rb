f4ce28c8478aee5b579baec2b6dee93060d9e7a2
Nayan Prakash <nayanprakash2@gmail.com>
2022-03-11 19:08:41 -0700

    restore main version of schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_03_10_153625) do
+ActiveRecord::Schema[7.1].define(version: 20_220_310_153_625) do
   create_table 'active_storage_attachments', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
+    t.integer 'record_id', null: false
+    t.integer 'blob_id', null: false
     t.datetime 'created_at', null: false
     t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index ['record_type', 'record_id', 'name', 'blob_id'], name: 'index_active_storage_attachments_uniqueness', unique: true
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
   create_table 'active_storage_blobs', force: :cascade do |t|
@@ -34,15 +34,15 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_10_153625) do
   end
 
   create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
+    t.integer 'blob_id', null: false
     t.string 'variation_digest', null: false
-    t.index ['blob_id', 'variation_digest'], name: 'index_active_storage_variant_records_uniqueness', unique: true
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
   create_table 'diffs', force: :cascade do |t|
     t.integer 'layer_id', null: false
     t.integer 'seq'
-    t.json 'data', default: '{}', null: false
+    t.json 'data', default: '""{}""', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['layer_id'], name: 'index_diffs_on_layer_id'
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_10_153625) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['notebook_id'], name: 'index_pages_on_notebook_id'
-    t.index ['number', 'notebook_id'], name: 'index_pages_on_number_and_notebook_id', unique: true
+    t.index %w[number notebook_id], name: 'index_pages_on_number_and_notebook_id', unique: true
   end
 
   create_table 'roles', force: :cascade do |t|
@@ -88,9 +88,9 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_10_153625) do
     t.integer 'resource_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['name', 'resource_type', 'resource_id'], name: 'index_roles_on_name_and_resource_type_and_resource_id'
+    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
     t.index ['name'], name: 'index_roles_on_name'
-    t.index ['resource_type', 'resource_id'], name: 'index_roles_on_resource'
+    t.index %w[resource_type resource_id], name: 'index_roles_on_resource'
   end
 
   create_table 'user_notebooks', force: :cascade do |t|
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_10_153625) do
     t.integer 'user_id'
     t.integer 'role_id'
     t.index ['role_id'], name: 'index_users_roles_on_role_id'
-    t.index ['user_id', 'role_id'], name: 'index_users_roles_on_user_id_and_role_id'
+    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
     t.index ['user_id'], name: 'index_users_roles_on_user_id'
   end
 
