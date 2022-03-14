4fc55eb8a5fba77187a2f9332b9579068fbfe2a1
Ryan Kortbeek <rakortbe@ualberta.ca>
2022-03-13 19:14:57 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_220_310_153_625) do
+ActiveRecord::Schema[7.1].define(version: 20_220_314_001_732) do
   create_table 'active_storage_attachments', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'record_type', null: false
-    t.integer 'record_id', null: false
-    t.integer 'blob_id', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
     t.datetime 'created_at', null: false
     t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
     t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 20_220_310_153_625) do
   end
 
   create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.integer 'blob_id', null: false
+    t.bigint 'blob_id', null: false
     t.string 'variation_digest', null: false
     t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.1].define(version: 20_220_310_153_625) do
     t.json 'data', default: '""{}""', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'visible', default: true, null: false
+    t.string 'type', null: false
     t.index ['layer_id'], name: 'index_diffs_on_layer_id'
   end
 
