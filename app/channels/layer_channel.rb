93fa5cc04676bcfd5ff35ef0e6aa29f78759f716
Ryan Kortbeek <rakortbe@ualberta.ca>
2022-03-15 00:35:20 -0600

    Implement server side persisting/processing of diff transmission

--- app/channels/layer_channel.rb
+++ app/channels/layer_channel.rb
@@ -1,26 +1,39 @@
+# TODO: Improve logging
+
 class LayerChannel < ApplicationCable::Channel
-  # TODO: store diffs broadcasted by owner client in db
-  # TODO: receive diffs from participant and store them in db
+  TANGIBLE_DIFF = 'tangible'.freeze
+  REMOVE_DIFF = 'remove'.freeze
+  TRANSLATE_DIFF = 'translate'.freeze
+
   def subscribed
     stream_from "layer_channel_#{params[:layer_id]}"
-
     # Send existing diffs for the layer
     ActionCable.server.broadcast("layer_channel_#{params[:layer_id]}",
-                                 Diff.where(layer_id: params[:layer_id]).order(:seq).pluck(:seq, :visible, :data))
+                                 Diff.where(layer_id: params[:layer_id]).order(:seq).pluck(:diff_type, :seq, :data, :visible))
   end
 
   def receive(data)
-    unless persist_diff(data)
-      # TODO: Handle scenario where save fails
-      puts 'Failed to save diff!'
+    # Always persist incoming diff
+    persist_diff(data)
+
+    # Apply possible side effects of incoming diff
+    diff_type = data['diff_type']
+    diff_data = data['data']
+
+    case diff_type
+    when REMOVE_DIFF
+      # Mark removed diffs as invisible
+      Diff.where(seq: diff_data).map { |diff| diff.update_attribute(:visible, false) }
+    when TRANSLATE_DIFF
+      # Update data of translated diffs
+      diff_data.each do |translated_diff|
+        diff = Diff.where(seq: translated_diff['seq'], layer_id: params[:layer_id]).first
+        diff&.update_attribute(:data, translated_diff['data'])
+      end
     end
-    puts 'Successfully saved diff!'
-    # TODO: refactor so 'Eraser' is not hardcoded
-    if data['type'].eql?('Eraser')
-      Diff.where(seq: data['data']).map(&diff.update_attribute(visible: false))
-      # TODO: rebroadcast if owner layer
-      ActionCable.server.broadcast("layer_channel_#{params[:layer_id]}", data)
-    end
+  rescue StandardError => e
+    # TODO: Handle scenario where save fails
+    puts "Failed to save and process incoming diff!\n#{e.inspect}"
   end
 
   def unsubscribed
@@ -30,23 +43,18 @@ class LayerChannel < ApplicationCable::Channel
   private
 
   def persist_diff(diff)
-    puts diff
-
-    diff_type = diff['type']
+    diff_type = diff['diff_type']
     diff_seq = diff['seq']
+    diff_visible = diff['visible']
     diff_data = diff['data']
     return false if diff_type.nil? || diff_seq.nil? || diff_data.nil?
 
     diff = Diff.new
-    diff.type = diff_type
+    diff.diff_type = diff_type
     diff.seq = diff_seq
     diff.data = diff_data
-    diff.visible = diff_type.eql?('Pen') || diff_type.eql?('Text')
+    diff.visible = diff_visible unless diff_visible.nil?
     diff.layer_id = params[:layer_id]
     diff.save!
-  rescue StandardError => e
-    # TODO: Improve logging
-    puts "Failed to save diff!\n#{e.inspect}"
-    false
   end
 end
