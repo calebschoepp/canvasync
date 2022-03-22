37aa648071d7c1e0bcf64582e009ce6651ad7439
Nayan Prakash <nayanprakash2@gmail.com>
2022-03-22 16:38:31 -0600

    fix migrations and schema formatting

--- db/migrate/20220310153625_create_exports.rb
+++ db/migrate/20220310153625_create_exports.rb
@@ -4,7 +4,6 @@ class CreateExports < ActiveRecord::Migration[7.1]
       t.references :notebook, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.boolean :ready
-      t.boolean :failed
 
       t.timestamps
     end
