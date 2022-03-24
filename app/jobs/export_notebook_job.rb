6f2b0d8c6c100299172509bbb9f16ea86d6ec737
Nayan Prakash <nayanprakash2@gmail.com>
2022-03-24 16:06:20 -0600

    fix bugs and align pdf generation

--- app/jobs/export_notebook_job.rb
+++ app/jobs/export_notebook_job.rb
@@ -6,6 +6,8 @@ class ExportNotebookJob < ApplicationJob
   include PageDimensions
   queue_as :default
 
+  SCALING_FACTOR = 1.5
+
   def perform(export_id)
     export = Export.find(export_id)
 
@@ -40,7 +42,8 @@ class ExportNotebookJob < ApplicationJob
           end
         end
       end
-    rescue StandardError
+    rescue StandardError => e
+      puts "Rescued: #{e.inspect}"
       export.failed = true
     else
       export.document.attach(io: File.open(tempfile.path), filename: "#{notebook.name}.pdf")
@@ -53,6 +56,10 @@ class ExportNotebookJob < ApplicationJob
   def count_pdf_pages(pdf_file_path)
     pdf = Prawn::Document.new(:template => pdf_file_path)
     pdf.page_count
+  end
+
+  def scaled_page_dimensions
+    PAGE_DIMS.map { |dim| dim * SCALING_FACTOR }
   end
 
   def set_transformation_matrix(pdf, filename)
@@ -69,9 +76,12 @@ class ExportNotebookJob < ApplicationJob
       if token.is_a?(PDF::Reader::Token) && PDF::Reader::PagesStrategy::OPERATORS.key?(token)
         operator = PDF::Reader::PagesStrategy::OPERATORS[token]
         if operator == :concatenate_matrix
+          params[0] = (params[0] * 1.1875)
+          params[3] = (params[3] * 1.1875)
+          params[5] = (params[5].to_f * (4.0 / 3.0)).round
           pdf.transformation_matrix(*params)
         else
-          pdf.transformation_matrix(1, 0, 0, 1, 0, 0)
+          pdf.transformation_matrix((2.0 / 3.0), 0, 0, (2.0 / 3.0), 0, 0)
         end
         # If first token was not a concatenate_matrix operator, then do nothing.
         return
@@ -92,9 +102,9 @@ class ExportNotebookJob < ApplicationJob
         if segments
           (1..(segments.length - 1)).each do |point|
             # get last point anchor
-            source = [segments[point - 1][0][0].to_f, PAGE_DIMS[1] - segments[point - 1][0][1].to_f]
+            source = [segments[point - 1][0][0].to_f, scaled_page_dimensions[1] - segments[point - 1][0][1].to_f]
             # get this point anchor
-            dest = [segments[point][0][0].to_f, PAGE_DIMS[1] - segments[point][0][1].to_f]
+            dest = [segments[point][0][0].to_f, scaled_page_dimensions[1] - segments[point][0][1].to_f]
             # get last point handle out + last point anchor to get first bezier anchor point
             bezier1 = [source[0] + segments[point - 1][2][0].to_f, source[1] - segments[point - 1][2][1].to_f]
             # get this point handle in + this point anchor to get second bezier anchor point
@@ -113,7 +123,7 @@ class ExportNotebookJob < ApplicationJob
         pdf.line_width 3
         pdf.stroke
       when 'PointText'
-        pdf.draw_text data[1]['content'], :at => [data[1]['matrix'][4].to_f, PAGE_DIMS[1] - data[1]['matrix'][5].to_f], :size => 25
+        pdf.draw_text data[1]['content'], :at => [data[1]['matrix'][4].to_f, scaled_page_dimensions[1] - data[1]['matrix'][5].to_f], :size => 25
       end
     end
   end
