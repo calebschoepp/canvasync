1f1d6007d9a944315f718cf7e71febf5288d13e4
Nayan Prakash <nayanprakash2@gmail.com>
2022-03-24 16:40:02 -0600

    add adjustments for landscape slides

--- app/jobs/export_notebook_job.rb
+++ app/jobs/export_notebook_job.rb
@@ -18,6 +18,7 @@ class ExportNotebookJob < ApplicationJob
 
     begin
       notebook.background.blob.open do |template|
+        orientation = pdf_orientation(template)
         Prawn::Document.generate(tempfile.path, :skip_page_creation => true, :margin => PAGE_MARGINS) do |pdf|
           (1..notebook.pages.length).each do |i|
             pdf.start_new_page :template => template, :template_page => i
@@ -31,13 +32,13 @@ class ExportNotebookJob < ApplicationJob
             page = notebook.pages.find_by(:number => i)
             if page && user_notebook.is_owner
               layer = page.layers.find_by(:writer => user_notebook)
-              draw_layer_diffs(pdf, layer)
+              draw_layer_diffs(pdf, layer, orientation)
             elsif page
               owner_user_notebook = notebook.user_notebooks.find_by(user_id: notebook.owner)
               owner_layer = page.layers.find_by(:writer => owner_user_notebook)
-              draw_layer_diffs(pdf, owner_layer)
+              draw_layer_diffs(pdf, owner_layer, orientation)
               participant_layer = page.layers.find_by(:writer => user_notebook)
-              draw_layer_diffs(pdf, participant_layer)
+              draw_layer_diffs(pdf, participant_layer, orientation)
             end
           end
         end
@@ -51,6 +52,11 @@ class ExportNotebookJob < ApplicationJob
     ensure
       export.save
     end
+  end
+
+  def pdf_orientation(filename)
+    pdf_reader = PDF::Reader.new(filename)
+    pdf_reader.pages.first.orientation
   end
 
   def count_pdf_pages(pdf_file_path)
@@ -91,7 +97,8 @@ class ExportNotebookJob < ApplicationJob
     end
   end
 
-  def draw_layer_diffs(pdf, layer)
+  def draw_layer_diffs(pdf, layer, orientation)
+    vertical_offset = orientation == 'portrait' ? scaled_page_dimensions[1] : scaled_page_dimensions[0]
     layer&.diffs&.each do |diff|
       next unless diff.diff_type == 'tangible' && diff.visible
 
@@ -102,9 +109,9 @@ class ExportNotebookJob < ApplicationJob
         if segments
           (1..(segments.length - 1)).each do |point|
             # get last point anchor
-            source = [segments[point - 1][0][0].to_f, scaled_page_dimensions[1] - segments[point - 1][0][1].to_f]
+            source = [segments[point - 1][0][0].to_f, vertical_offset - segments[point - 1][0][1].to_f]
             # get this point anchor
-            dest = [segments[point][0][0].to_f, scaled_page_dimensions[1] - segments[point][0][1].to_f]
+            dest = [segments[point][0][0].to_f, vertical_offset - segments[point][0][1].to_f]
             # get last point handle out + last point anchor to get first bezier anchor point
             bezier1 = [source[0] + segments[point - 1][2][0].to_f, source[1] - segments[point - 1][2][1].to_f]
             # get this point handle in + this point anchor to get second bezier anchor point
@@ -123,7 +130,7 @@ class ExportNotebookJob < ApplicationJob
         pdf.line_width 3
         pdf.stroke
       when 'PointText'
-        pdf.draw_text data[1]['content'], :at => [data[1]['matrix'][4].to_f, scaled_page_dimensions[1] - data[1]['matrix'][5].to_f], :size => 25
+        pdf.draw_text data[1]['content'], :at => [data[1]['matrix'][4].to_f, vertical_offset - data[1]['matrix'][5].to_f], :size => 25
       end
     end
   end
