e83d043c5db595670ee72274725e1b63b06ec1dd
Nayan Prakash <nayanprakash2@gmail.com>
2022-03-27 22:10:02 -0600

    work on react tests

--- test/javascript/App.test.js
+++ test/javascript/App.test.js
@@ -5,25 +5,47 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import App from "../../app/javascript/components/App";
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { createConsumer } from "@rails/actioncable";
 
-jest.mock("@rails/actioncable", () => {
-  return {
-    createConsumer: jest.fn().mockImplementation(() => {
-      return {};
-    }),
-  };
-});
+describe('react canvas app tests', () => {
+  beforeEach(() => {
+    jest.mock("@rails/actioncable", () => {
+      return {
+        createConsumer: jest.fn().mockImplementation(() => {
+          return {};
+        }),
+      };
+    });
+  
+    window.notebookId = 1;
+    window.notebookName = "Notebook Name";
+    window.isOwner = true;
+    window.ownerLayers = [];
+    window.participantLayers = [];
+    window.currentUser = 1;
+    window.ownerId = 1;
+    window.userNotebookId = 1;
+    window.backgroundPdf = null;
+  });
 
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  createConsumer.mockClear();
-
-  window.participantLayers = [];
-  window.ownerLayers = [];
-});
-
-it("renders correctly", () => {
-  const app = renderer.create(<App />).toJSON();
-  expect(app).toMatchSnapshot();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  it("renders correctly", () => {
+    const app = renderer.create(<App />);
+    expect(app).toMatchSnapshot();
+  });
+  
+  it("renders a new page when page added", () => {
+    const app = renderer.create(<App />);
+    expect(app).toMatchSnapshot();
+    const instance = app.root;
+    const newPageButton = instance.findByProps({icon: faPlus}).parent;
+    console.log(newPageButton.props);
+    newPageButton.props.onClick();
+    expect(app).toMatchSnapshot();
+    expect(window.ownerLayers.length).toBe(1);
+  });
 });
