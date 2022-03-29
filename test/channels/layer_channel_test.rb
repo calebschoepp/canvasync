99b8ac0e7376ed88ee78e291f614fb02a547dd0d
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-03-29 16:11:37 -0600

    IT WORKS!

--- test/channels/layer_channel_test.rb
+++ test/channels/layer_channel_test.rb
@@ -17,4 +17,27 @@ class LayerChannelTest < ActionCable::Channel::TestCase
 
     assert subscription.rejected?
   end
+
+  test 'todo rename me' do
+    subscribe layer_id: Layer.first.id
+
+    expected_msg = { 'diff_type' => 'fetch-existing',
+                     'data' =>
+                      [{ 'diff_type' => 'tangible',
+                         'seq' => 0,
+                         'data' =>
+                          '["Path",{"applyMatrix":true,"segments":[[[250,282],[0,0],[48.14208,-48.14208]],[[388,306],[-44.79859,-10.33814],[53.0259,12.23675]],[[527,251],[-34.26694,37.69363],[0,0]]],"strokeColor":[0,0,0],"strokeWidth":3}]',
+                         'visible' => true },
+                       { 'diff_type' => 'tangible',
+                         'seq' => 2,
+                         'data' =>
+                          '["PointText",{"applyMatrix":false,"matrix":[1,0,0,1,549,594],"content":"some text","strokeColor":[0,0,0],"fontSize":25,"leading":30}]',
+                         'visible' => true }],
+                     'next_seq' => 5 }
+
+    assert_broadcast_on("layer_channel_#{Layer.first.id}", expected_msg) do
+      subscription.receive({ 'diff_type' => 'fetch-existing', 'seq' => 0 })
+    end
+
+  end
 end
