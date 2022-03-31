c3ccc44853a188c3fca2e54d46c2e22364626b23
Caleb <caleb.schoepp@gmail.com>
2022-03-31 12:05:12 -0600

    Revert "Remove compressor"
    This reverts commit 507afb7adcada9ffe1757a9bef24b218e8db9db7.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Canvasync
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.assets.css_compressor = nil
+    config.assets.css_compressor = nil
   end
 end
