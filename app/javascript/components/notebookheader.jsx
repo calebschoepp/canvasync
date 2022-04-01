e4927da6644929f8d1e9ede477b596de519b190a
Nayan Prakash <43127491+nayancprakash@users.noreply.github.com>
2022-04-01 11:57:05 -0600

    add bug fixes (#46)

--- app/javascript/components/notebookheader.jsx
+++ app/javascript/components/notebookheader.jsx
@@ -4,15 +4,37 @@ export const NotebookHeader = () => {
 
   const handleCopyButtonCallback = useCallback((event) => {
     const copyUrl = window.location.href + '/preview';
-    navigator.clipboard.writeText(copyUrl).then(() => {
-      event.target.classList.add('copied');
-      const copiedNotificationTimeout = setInterval(() => {
-        event.target.classList.remove('copied');
-        clearInterval(copiedNotificationTimeout);
-      }, 600);
-    }, () => {
-      /* clipboard write failed */
-    });
+    if (navigator.clipboard && window.isSecureContext) {
+      navigator.clipboard.writeText(copyUrl).then(() => {
+        event.target.classList.add('copied');
+        const copiedNotificationTimeout = setInterval(() => {
+          event.target.classList.remove('copied');
+          clearInterval(copiedNotificationTimeout);
+        }, 600);
+      }, () => {
+        /* clipboard write failed */
+      });
+    } else {
+      const textArea = document.createElement("textarea");
+      textArea.value = copyUrl;
+      // make the textarea out of viewport
+      textArea.style.position = "fixed";
+      textArea.style.left = "-999999px";
+      textArea.style.top = "-999999px";
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      return new Promise((res, rej) => {
+        // here the magic happens
+        document.execCommand('copy') ? res() : rej();
+        textArea.remove();
+        event.target.classList.add('copied');
+        const copiedNotificationTimeout = setInterval(() => {
+          event.target.classList.remove('copied');
+          clearInterval(copiedNotificationTimeout);
+        }, 600);
+      });
+    }
   });
 
   return (
