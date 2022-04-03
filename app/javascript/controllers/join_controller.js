5b271220bfcdf9e9cf6fa7d0b991f778fac35a19
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-04-03 13:46:33 -0600

    Show error when giving invalid notebook id

--- app/javascript/controllers/join_controller.js
+++ app/javascript/controllers/join_controller.js
@@ -6,11 +6,21 @@ export default class extends Controller {
 
   connect() {}
 
-  // TODO: Display an error if id is invalid
   preview() {
     const id = this.id;
     if (id && /^\d+$/.test(id)) {
-      window.location.href = `/notebooks/${this.id}/preview`;
+        const url = `/notebooks/${this.id}/preview`;
+        fetch(url)
+            .then(response => response.status)
+            .then(status => {
+                if (status === 404) {
+                    alert("Notebook does not exist");
+                } else {
+                    window.location.href = url;
+                }
+            });
+    } else {
+        alert("Invalid notebook id");
     }
   }
 
